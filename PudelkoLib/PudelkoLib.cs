1fa6b2d4c320191d73ccf1b44ddba61edb402648
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-04-12 23:40:03 +0200

    commit startowy

---
+++ PudelkoLib/PudelkoLib.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+
+
+namespace PudelkoLibrary
+{
+    //Reprezentujący jednostki miary enum o nazwie UnitOfMeasure
+    public enum UnitOfMeasure
+    {
+        milimetr, 
+        centimetr,
+        meter
+    }
+
+    public class PudelkoLib
+    {
+
+    }
+}
--- UnitTestPudelko/UnitTestPudelko.cs
+++ UnitTestPudelko/UnitTestPudelko.cs
@@ -1,4 +1,4 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PudelkoLibrary;
 using System;
 using System.Collections.Generic;
