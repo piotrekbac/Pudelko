5ce8bb49273bb9a541cc24b162a050d65ae664f2
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-04-13 00:01:53 +0200

    commit 1

--- PudelkoLib/PudelkoLib.cs
+++ PudelkoLib/PudelkoLib.cs
@@ -12,9 +12,19 @@ namespace PudelkoLibrary
         centimetr,
         meter
     }
-
-    public class PudelkoLib
+    
+    //Klasa pudełko reprezentująca nasze trójwymiarowe pudełko 
+    //Używamy sealed class aby zablkokować dziedziczenie klasy
+    public sealed class PudelkoLib : IEquatable<PudelkoLib>, IEnumerable, IFormattable
     {
+        //Pola przechowujące wymiary pudełka w metrach 
+        private readonly double a;
+        private readonly double b;
+        private readonly double c;
 
+        //Teraz ustawiamy właściwości dostępu do wymiarów pudełka (zaokrąglone do trzech miejsc po przecinku)
+        public double A => Math.Round(a, 3);
+        public double B => Math.Round(b, 3);
+        public double C => Math.Round(c, 3);
     }
 }
